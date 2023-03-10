358e32c6e28022af905df1ea607078cbb6548d2a
TriNguyen8888 <tringuyen8107@gmail.com>
2023-03-10 21:28:56 +0700

    lor

---
+++ main.js
@@ -0,0 +1,16 @@
+const wrapper = document.querySelector('.wrapper');
+const login = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
